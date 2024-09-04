7329c52fc5abc1bbca90ca984df66979c5e9cf03
Prof Vinicius <gvweb.solucoes@gmail.com>
2024-09-04 19:55:09 -0300

    Arquivo Teste Alterado

--- teste.js
+++ teste.js
@@ -5,21 +5,3 @@ let Cestafrutas = {
     "nome": "Uva", 
     "preco": 12.00 
 }
-
-console.log(Cestafrutas.nome);
-
-let CestafrutasDois = [
-    {"Nome": "Uva", "preco": 12.00}, 
-    {"Nome": "Maca", "preco": 13.00}, 
-    {"Nome": "Banana", "preco": 14.00}, 
-]
-
-console.log(CestafrutasDois[2].Nome);
-
-frutas.forEach(e => {
-    console.log(e);
-});
-
-for (let index = 0; index < frutas.length; index++) {
-    console.log(frutas[index]);
-}
