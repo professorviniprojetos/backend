91d350b8ec3047c7a71295056aa1bd0e7aaed219
Prof Vinicius <gvweb.solucoes@gmail.com>
2024-09-11 19:02:29 -0300

    Conexão com prisma

--- src/routes.ts
+++ src/routes.ts
@@ -3,11 +3,7 @@ import {Router, Request, Response} from 'express';
 const router = Router();
 
 router.get('/teste', (req: Request, res: Response) => {
-    return res.json(
-        {
-            ok: true
-        }
-    )
+    throw new Error('Error ao fazer essa requisição');
 });
 
 export {router};
